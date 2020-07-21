b9eaa3255c12c3fc889e8aeda83493a29af78892
chotto-wavy <keitoboy1999@gmail.com>
2020-07-21 18:43:08 +0900

    linkのパスが間違ってたから直した

--- php_files/index.php
+++ php_files/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-    <link rel="stylesheet" href="css_files/stylesheet.css">
+    <link rel="stylesheet" href="../css_files/stylesheet.css">
 </head>
 <body>
 <header class="leftNavigation">
