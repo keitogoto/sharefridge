0957bd44f0753d8356e1ac6ac195681c7c1d3386
chotto-wavy <keitoboy1999@gmail.com>
2020-07-21 17:22:32 +0900

    html_fileを削除し、php_filesのみで作成する

--- php_files/index.php
+++ php_files/index.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="css_files/stylesheet.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<!-- <?php
 print"sharefridge"
-?>
+?> -->
