0b7f85ea604eacfcaae670751b652e0f8af4b023
chotto-wavy <keitoboy1999@gmail.com>
2020-07-24 12:48:39 +0900

    ログイン新規登録画面作成

--- php_files/login.php
+++ php_files/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-    <link rel="stylesheet" href="../css_files/stylesheet.css">
+    <link rel="stylesheet" href="../css_files/login.css">
 </head>
 
 <body>
