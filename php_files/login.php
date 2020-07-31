6a03451ad28df3064e0d716ce3eff17ab6148d96
yujialves <yujialves@i.softbank.jp>
2020-07-31 22:17:53 +0900

    コードの整理

--- php_files/login.php
+++ php_files/login.php
@@ -1,30 +1,23 @@
 <?php
 
-// $user = $_POST["fridge_id"];
 $fridge = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
 try{
-    //$dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "$user", "$password");
+    // ハッシュ化パスワードの抽出
     $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
-
-    // $stmt = $dbh->prepare('SELECT * FROM users WHERE fridge_id = :fridge_id');
     $stmt = $dbh->prepare('SELECT * FROM fridges WHERE fridge_id = :fridge_id');
-
     $stmt->execute(array(':fridge_id' => $_POST['fridge_id']));
-
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    // ログイン認証
     if(password_verify($_POST['pass'], $result['password'])){
         echo "ログイン認証に成功しました";
     }else{
         echo "ログイン認証に失敗しました";
     } 
-
 }catch(Exception $e){
     echo "データベースの接続に失敗しました：";
     echo $e->getMessage();
     die();
 }
-
-?>
--- php_files/register.php
+++ php_files/register.php
@@ -1,34 +1,19 @@
 <?php
 
-// $user = $_POST["fridge_id"];
 $fridge = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
-// echo $_POST["fridge_id"];
-// echo $_POST["pass"];
+try {
+    // Fridgeの登録
+    $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
+    $stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
+    $stmt->execute(array(':fridge_id' => $fridge, ':password' => password_hash($password, PASSWORD_DEFAULT)));
 
-try{
-
-// $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "$user", "$password");
-$dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
-
-// $stmt = $dbh->prepare("INSERT INTO users (fridge_id, password) VALUES (:fridge_id, :password)");
-$stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
-
-$stmt->execute(array(':fridge_id' => $_POST['fridge_id'],':password' => password_hash($_POST['pass'], PASSWORD_DEFAULT)));
-
-//リダイレクト処理
-header('Location: ../html_files/login.html');
-exit;
-
-}catch(Exception $e){
+    // リダイレクト処理
+    header('Location: ../html_files/login.html');
+    exit;
+} catch (Exception $e) {
     echo "データベースの接続に失敗しました：";
     echo $e->getMessage();
     die();
 }
-
-// //リダイレクト処理
-// header('Location: ../html_files/login.html');
-// exit;
-
-?>
