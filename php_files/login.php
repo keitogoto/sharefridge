4ec0295affbd3549f9afad46322c1e918e8744cc
chotto-wavy <keitoboy1999@gmail.com>
2020-07-31 14:42:54 +0900

    ログイン新規登録画面作成

--- php_files/login.php
+++ php_files/login.php
@@ -1,15 +1,16 @@
 <?php
 
-$user = $_POST["user_id"];
+$user = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
 try{
     //$dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "$user", "$password");
     $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "keito", "0531");
 
-    $stmt = $dbh->prepare('SELECT * FROM users WHERE user_id = :user_id');
+    // $stmt = $dbh->prepare('SELECT * FROM users WHERE fridge_id = :fridge_id');
+    $stmt = $dbh->prepare('SELECT * FROM fridges WHERE fridge_id = :fridge_id');
 
-    $stmt->execute(array(':user_id' => $_POST['user_id']));
+    $stmt->execute(array(':fridge_id' => $_POST['fridge_id']));
 
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
--- php_files/register.php
+++ php_files/register.php
@@ -1,6 +1,6 @@
 <?php
 
-$user = $_POST["user_id"];
+$user = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
 try{
@@ -8,9 +8,10 @@ try{
 // $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "$user", "$password");
 $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "keito", "0531");
 
-$stmt = $dbh->prepare("INSERT INTO users (user_id, password) VALUES (:user_id, :password)");
+// $stmt = $dbh->prepare("INSERT INTO users (fridge_id, password) VALUES (:fridge_id, :password)");
+$stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
 
-$stmt->execute(array(':user_id' => $_POST['user_id'],':password' => password_hash($_POST['pass'], PASSWORD_DEFAULT)));
+$stmt->execute(array(':fridge_id' => $_POST['fridge_id'],':password' => password_hash($_POST['pass'], PASSWORD_DEFAULT)));
 
 }catch(Exception $e){
     echo "データベースの接続に失敗しました：";
