5f3d1721bbcbd76fa3fd92240b20224a0cbeaf3f
chotto-wavy <keitoboy1999@gmail.com>
2020-07-31 15:46:59 +0900

    ログイン新規登録画面作成

--- php_files/register.php
+++ php_files/register.php
@@ -4,13 +4,13 @@
 $fridge = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
-echo $_POST["fridge_id"];
-echo $_POST["pass"];
+// echo $_POST["fridge_id"];
+// echo $_POST["pass"];
 
 try{
 
 // $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", "$user", "$password");
-$dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', 0531);
+$dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
 
 // $stmt = $dbh->prepare("INSERT INTO users (fridge_id, password) VALUES (:fridge_id, :password)");
 $stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
