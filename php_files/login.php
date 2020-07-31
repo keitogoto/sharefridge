c1e70b2c72939267228404070faf57250bbdec6c
yujialves <yujialves@i.softbank.jp>
2020-07-31 22:29:31 +0900

    セッション処理の作成&fridge画面へのリダイレクト

--- php_files/login.php
+++ php_files/login.php
@@ -1,22 +1,28 @@
 <?php
+session_start();
 
 $fridge = $_POST["fridge_id"];
 $password = $_POST["pass"];
 
-try{
+try {
     // ハッシュ化パスワードの抽出
     $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
     $stmt = $dbh->prepare('SELECT * FROM fridges WHERE fridge_id = :fridge_id');
-    $stmt->execute(array(':fridge_id' => $_POST['fridge_id']));
+    $stmt->execute(array(':fridge_id' => $fridge));
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     // ログイン認証
-    if(password_verify($_POST['pass'], $result['password'])){
-        echo "ログイン認証に成功しました";
-    }else{
+    if (password_verify($password, $result['password'])) {
+
+        // fridge_idをセッションに格納
+        $_SESSION["fridge_id"] = $fridge;
+
+        // リダイレクト処理
+        header('Location: ../php_files/fridge.php');
+    } else {
         echo "ログイン認証に失敗しました";
-    } 
-}catch(Exception $e){
+    }
+} catch (Exception $e) {
     echo "データベースの接続に失敗しました：";
     echo $e->getMessage();
     die();
