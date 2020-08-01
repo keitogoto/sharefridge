a586cc914becf62d019d4d2fcded3f4439c7a046
chotto-wavy <keitoboy1999@gmail.com>
2020-08-01 11:28:58 +0900

    register.phpの実装

---
+++ php_files/fridge.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fridge-menu</title>
+    <link rel="stylesheet" href="../css_files/fridge.css">
+</head>
+
+<body>
+    <header>
+        <a href="index.html">
+            <div class="logo-wrapper">
+                <img class="fridge-img" src="../images/fridge.png">
+                <p class="logo">sharefridge</p>
+            </div>
+        </a>
+    </header>
+    <div class="content">
+        <table>
+            <tr>
+                <th>お名前</th>
+                <th>特技</th>
+                <th>担当カラー</th>
+                <th>住所</th>
+            </tr>
+            <tr>
+                <td class="icon bird">トリさん</td>
+                <td>空を飛ぶこと</td>
+                <td>グリーン</td>
+                <td>山</td>
+            </tr>
+            <tr>
+                <td class="icon whale">クジラさん</td>
+                <td>潮を吹くこと</td>
+                <td>ブルー</td>
+                <td>海</td>
+            </tr>
+            <tr>
+                <td class="icon crab">カニさん</td>
+                <td>反復横飛び</td>
+                <td>レッド</td>
+                <td>川</td>
+            </tr>
+        </table>
+
+        <!-- <div class="button-wrapper"> -->
+        <!-- ログイン画面へ遷移 -->
+        <a class="to_login" href="login.html">LOGIN</a>
+        <!-- 新規登録画面へ遷移 -->
+        <a class="to_register" href="register.html">REGISTER</a>
+        <!-- </div> -->
+    </div>
+</body>
+
+</html>
