3decede01bdfe4d2a9b373335f2f85ea7b5056f5
chotto-wavy <keitoboy1999@gmail.com>
2020-08-01 10:39:57 +0900

    register.phpの実装

--- php_files/register.php
+++ php_files/register.php
@@ -1,39 +1,21 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+$fridge = $_POST["fridge_id"];
+$password = $_POST["pass"];
 
-    <?php
+try {
+    // Fridgeの登録
+    $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
+    $stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
+    $stmt->execute(array(':fridge_id' => $fridge, ':password' => password_hash($password, PASSWORD_DEFAULT)));
 
-    $fridge = $_POST["fridge_id"];
-    $password = $_POST["pass"];
+    // リダイレクト処理
+    header('Location: ../html_files/login.html');
+    exit;
+} catch (Exception $e) {
+    echo "データベースの接続に失敗しました：";
+    echo $e->getMessage();
+    die();
+}
 
-    try {
-        // Fridgeの登録
-        $dbh = new PDO("mysql:host=localhost; dbname=sharefridge; charset=utf8", 'keito', '0531');
-        $stmt = $dbh->prepare("INSERT INTO fridges (fridge_id, password) VALUES (:fridge_id, :password)");
-        $stmt->execute(array(':fridge_id' => $fridge, ':password' => password_hash($password, PASSWORD_DEFAULT)));
-
-        // リダイレクト処理
-        header('Location: ../html_files/login.html');
-        exit;
-    } catch (Exception $e) {
-        echo "データベースの接続に失敗しました：";
-        echo $e->getMessage();
-        die();
-    }
-
-    ?>
-
-</head>
-
-<body>
-
-
-
-</body>
-
-</html>
+?>
